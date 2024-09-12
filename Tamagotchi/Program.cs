4685ef0cf91e20fd29523feb46d0eab6d9fda12b
Grandswed1 <elliot.rydell@elev.ga.ntig.se>
2024-09-12 16:13:53 +0200

    TheBeginning

---
+++ Tamagotchi/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Welcome!");
+Console.WriteLine("Try to take good care of this thing.");
+
+littlething myThing = new littlething();
+
+Console.WriteLine("Write what you would like to name it.");
+myThing.name = Console.ReadLine();
+
+Console.WriteLine($"Coolio! {myThing.name} is a really coolio name!");
+
+while (myThing.GetAlive() == true)
+{
+
+}
---
+++ Tamagotchi/littlething.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+    public class littlething
+    {
+        private int starvation;
+        private int boredom;
+        private bool isAlive;
+        public string name;
+
+        public littlething()
+        {
+            isAlive = true;
+        }
+    }
+
