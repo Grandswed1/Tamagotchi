04aaad4e8091852949c2bf050e5f75a1be98d476
Grandswed1 <elliot.rydell@elev.ga.ntig.se>
2024-09-19 11:59:27 +0200

    Finished?

--- Tamagotchi/Program.cs
+++ Tamagotchi/Program.cs
@@ -10,5 +10,42 @@ Console.WriteLine($"Coolio! {myThing.name} is a really coolio name!");
 
 while (myThing.GetAlive() == true)
 {
+    Console.Clear();
+    myThing.PrintStats();
+    Console.WriteLine("How would you lke to proceed?");
+    Console.WriteLine($"1. Try to teach {myThing.name} a new little word");
+    Console.WriteLine($"2. Try to talk to {myThing.name}");
+    Console.WriteLine($"3. Try to make {myThing.name} starve less");
+    Console.WriteLine($"4. Don't do anything");
+
+    string doWhat = Console.ReadLine();
+    if (doWhat == "1")
+    {
+        Console.WriteLine("What word would you like to teach it?");
+        string word = Console.ReadLine();
+        myThing.Teach(word);
+    }
+
+    if (doWhat == "2")
+    {
+        myThing.Hi();
+    }
+
+    if (doWhat == "3")
+    {
+        myThing.Feed();
+    }
+    else
+    {
+        Console.WriteLine("Doing nothing...");
+    } 
+
+    myThing.Tick();
+    Console.WriteLine("Press Enter to continue");
+    Console.ReadLine();
 
 }
+
+Console.WriteLine($"SHIT!! {myThing.name} died!");
+Console.WriteLine("Press enter to exit");
+Console.ReadLine();
--- Tamagotchi/littlething.cs
+++ Tamagotchi/littlething.cs
@@ -1,16 +1,77 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+using System.Xml;
 
-    public class littlething
+public class littlething
+{
+    private int starvation;
+    private int boredom;
+    public bool isAlive;
+    private List<string> words = new List<string>() { "Idiot" };
+    public string name;
+
+    public littlething()
     {
-        private int starvation;
-        private int boredom;
-        private bool isAlive;
-        public string name;
+        isAlive = true;
+    }
 
-        public littlething()
+
+    public void Feed()
+    {
+        Console.WriteLine($"[{name}] eats and is not hungry");
+        starvation -= 2;
+        if (starvation < 0)
         {
-            isAlive = true;
+            starvation = 0;
         }
     }
 
+    public void Hi()
+    {
+        int wordNum = Random.Shared.Next(words.Count);
+        Console.WriteLine($" [{name}] says: {words[wordNum]}");
+        ReduceBoredom();
+    }
+
+    public void Teach(string word)
+    {
+        Console.WriteLine("I am learning " + word);
+        words.Add(word);
+        ReduceBoredom();
+    }
+
+    public void Tick()
+    {
+        starvation++;
+        boredom++;
+        if (starvation > 5 || boredom > 5)
+        {
+            isAlive = false;
+        }
+    }
+
+    public void PrintStats()
+    {
+        Console.WriteLine("My name is " + name);
+        Console.WriteLine("My hunger is " + starvation);
+        Console.WriteLine("My boredome is "+ boredom);
+        Console.WriteLine("I know " + words.Count + " words");
+    }
+
+    public bool GetAlive()
+    {
+        return isAlive;
+    }
+
+    public void ReduceBoredom()
+    {
+        Console.WriteLine($" [{name}] is now a little less bored");
+        boredom -=2;
+        if (boredom < 0)
+        {
+            boredom = 0;
+        } 
+    }
+
+}
